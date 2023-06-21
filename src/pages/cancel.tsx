563c5bef141e06a851cc8d4ea5f8e7c1283875ac
minhthu0204 <phulinhnguyen3@gmail.com>
2023-06-22 00:22:34 +0700

    haha

---
+++ src/pages/cancel.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+
+const Cancel: NextPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h1>
+      Hủy thanh toán!
+      </h1>
+    </div>
+  );
+};
+
+export default Cancel;
